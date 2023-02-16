43a65ec4c9d3667a69863c1eb9e689dbca330ea8
vaibhav-kadu <vkdon02@gmail.com>
2023-02-16 18:30:00 +0530

    chore(release): prepare Version 1.0.3 (Code 3) for Play Store deployment
    - Align Version Code to 3 and Version Name to 1.0.3 for consistency
    
    - Finalize Admin Dashboard with dedicated Users tab and detail views
    
    - Streamline Signup UI by removing labels and compacting role selectors
    
    - Optimize production build configuration for successful signed AAB generation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = 36   // Play Store requirement (34+)
 
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.3"
     }
 
     signingConfigs {
